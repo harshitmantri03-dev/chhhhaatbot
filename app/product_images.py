f32be767ecc1f117bc8d861f554eea5e01bb6568
harshitmantri03-dev <harshitmantri03@gmail.com>
2026-09-23 16:21:51 +0530

    Update product_images.py

--- app/product_images.py
+++ app/product_images.py
@@ -6,9 +6,9 @@
 PRODUCT_IMAGES = {
     "ANTIQUE": [
         "https://botspace-uploads.s3.eu-west-1.amazonaws.com/6a7d9659715d2fa28d23f4aa/links/57c44bd2-4dd8-46de-b3e7-f44fee18d2fa.jpg",
-        "https://drive.google.com/uc?export=view&id=1KPMKFGayiMUog4zYlsmt-SLYfDl5iObR",
-        "https://drive.google.com/uc?export=view&id=1EgD8Ir-iYC4C9ee__1lxLqeH97HWzd99",
-        "https://drive.google.com/uc?export=view&id=1UWQerSB_3SPA2lo1i7C8a8aEkvKDAEiZ",
+        "https://botspace-uploads.s3.eu-west-1.amazonaws.com/6a7d9659715d2fa28d23f4aa/links/60692e74-ec01-4ca3-a5e7-e61bbfc47dd4.jpg",
+        "https://botspace-uploads.s3.eu-west-1.amazonaws.com/6a7d9659715d2fa28d23f4aa/links/74636fc1-28e9-4cb2-9a4b-0c8173f33331.jpg",
+        "https://botspace-uploads.s3.eu-west-1.amazonaws.com/6a7d9659715d2fa28d23f4aa/links/87c44955-5527-41bc-8e97-ef1de422d905.jpg",
     ],
     "BRACELET_MEN": [
         "https://your-image-link-6.jpg",
