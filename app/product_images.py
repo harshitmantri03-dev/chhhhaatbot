12bedece25ecb732386ce3cb926f69a1cba5a82b
harshitmantri03-dev <harshitmantri03@gmail.com>
2026-09-23 16:27:16 +0530

    Update product_images.py

--- app/product_images.py
+++ app/product_images.py
@@ -5,7 +5,7 @@
 
 PRODUCT_IMAGES = {
     "ANTIQUE": [
-        "https://botspace-uploads.s3.eu-west-1.amazonaws.com/6a7d9659715d2fa28d23f4aa/links/57c44bd2-4dd8-46de-b3e7-f44fee18d2fa.jpg",
+        "https://botspace-uploads.s3.eu-west-1.amazonaws.com/6a7d9659715d2fa28d23f4aa/links/068fac84-a99f-4677-ad2b-c8c2fbc4cec0.jpg",
         "https://botspace-uploads.s3.eu-west-1.amazonaws.com/6a7d9659715d2fa28d23f4aa/links/60692e74-ec01-4ca3-a5e7-e61bbfc47dd4.jpg",
         "https://botspace-uploads.s3.eu-west-1.amazonaws.com/6a7d9659715d2fa28d23f4aa/links/74636fc1-28e9-4cb2-9a4b-0c8173f33331.jpg",
         "https://botspace-uploads.s3.eu-west-1.amazonaws.com/6a7d9659715d2fa28d23f4aa/links/87c44955-5527-41bc-8e97-ef1de422d905.jpg",
